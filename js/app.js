e0102794207c5ce07db9b6e3bb812c01caead629
ronofri11 <renato.onofri@foris.cl>
2015-04-24 18:42:48 -0300

    replace wreqr with backbone radio

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 define([
-    "marionette"
-], function (Marionette) {
-    App = new Marionette.Application();
-    App.listenTo(App, "start", function(){
-        alert("Hello World!");
-    });
+	"backbone.marionette",
+	"backbone.radio",
+	"radio.shim",
+], function (Marionette, Radio, Shim) {
+
+	var App = new Marionette.Application();
+
+	var channel = Radio.channel('global');
+    console.log(channel);
+
+
+    // App = new Marionette.Application();
+    // App.listenTo(App, "start", function(){
+    //     alert("Hello World!");
+    // });
     return App;
 });
--- js/main.js
+++ js/main.js
@@ -1,41 +1,24 @@
 "use strict";
 
 require.config({
-    shim: {
-        jquery : {
-          exports : 'jquery'
-        },
-        underscore : {
-          exports : '_'
-        },
-        backbone : {
-          deps : ['jquery', 'underscore'],
-          exports : 'Backbone'
-        },
-        wreqr: {
-          deps : ['backbone']
-        },
-        babysitter : {
-          deps: ['backbone']
-        },
-        marionette : {
-          deps: ['wreqr', 'babysitter'],
-          exports : 'Marionette'
-        }
-    },
-    // baseUrl: "/maintainers/bower_components/",
     paths : {
         backbone : "../bower_components/backbone/backbone",
         underscore : "../bower_components/underscore/underscore",
         jquery : "../bower_components/jquery/dist/jquery",
-        marionette : "../bower_components/backbone.marionette/lib/backbone.marionette",
-        wreqr : "../bower_components/backbone.wreqr/lib/backbone.wreqr",
-        babysitter : "../bower_components/backbone.babysitter/lib/backbone.babysitter",
+        "backbone.marionette" : "../bower_components/backbone.marionette/lib/core/backbone.marionette",
+        "backbone.radio" : "../bower_components/backbone.radio/build/backbone.radio",
+        "backbone.babysitter" : "../bower_components/backbone.babysitter/lib/backbone.babysitter",
         text: "../bower_components/requirejs-text/text"
+    },
+    enforceDefine: true,
+    map: {
+        '*': {
+            'backbone.wreqr': 'backbone.radio'
+        } 
     }
 });
 
-require([
+define([
     "app"
 ], function (App) {
     App.start();
